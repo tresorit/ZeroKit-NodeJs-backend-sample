389e4b7f5ca5850248e078343978b95fc6f575e7
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2017-07-03 15:27:48 +0200

    Adding test helpers to all opened windows

--- test/lib/test.js
+++ test/lib/test.js
@@ -20,149 +20,154 @@ require('chai').should();
 const idpCodeRedirect = conf.zeroKit.idp.find((a) => a.clientID === process.env["ZKIT_CODE_CLIENT_ID"]).callbackURL;
 const idpHybridRedirect = conf.zeroKit.idp.find((a) => a.clientID === process.env["ZKIT_HYBRID_CLIENT_ID"]).callbackURL;
 
+function setupHelpers(remote) {
+  return remote.execute(function () {
+    function bytes_to_base64(arr){
+      return btoa( bytes_to_string(arr) );
+    }
+
+    function base64_to_bytes(str){
+      return string_to_bytes( atob( str ) );
+    }
+
+    function string_to_bytes ( str, utf8 ) {
+      utf8 = !!utf8;
+
+      var len = str.length,
+        bytes = new Uint8Array( utf8 ? 4*len : len );
+
+      for ( var i = 0, j = 0; i < len; i++ ) {
+        var c = str.charCodeAt(i);
+
+        if ( utf8 && 0xd800 <= c && c <= 0xdbff ) {
+          if ( ++i >= len ) throw new Error( "Malformed string, low surrogate expected at position " + i );
+          c = ( (c ^ 0xd800) << 10 ) | 0x10000 | ( str.charCodeAt(i) ^ 0xdc00 );
+        }
+        else if ( !utf8 && c >>> 8 ) {
+          throw new Error("Wide characters are not allowed.");
+        }
+
+        if ( !utf8 || c <= 0x7f ) {
+          bytes[j++] = c;
+        }
+        else if ( c <= 0x7ff ) {
+          bytes[j++] = 0xc0 | (c >> 6);
+          bytes[j++] = 0x80 | (c & 0x3f);
+        }
+        else if ( c <= 0xffff ) {
+          bytes[j++] = 0xe0 | (c >> 12);
+          bytes[j++] = 0x80 | (c >> 6 & 0x3f);
+          bytes[j++] = 0x80 | (c & 0x3f);
+        }
+        else {
+          bytes[j++] = 0xf0 | (c >> 18);
+          bytes[j++] = 0x80 | (c >> 12 & 0x3f);
+          bytes[j++] = 0x80 | (c >> 6 & 0x3f);
+          bytes[j++] = 0x80 | (c & 0x3f);
+        }
+      }
+
+      return bytes.subarray(0, j);
+    }
+
+    function bytes_to_string ( bytes, utf8 ) {
+      utf8 = !!utf8;
+
+      var len = bytes.length,
+        chars = new Array(len);
+
+      for ( var i = 0, j = 0; i < len; i++ ) {
+        var b = bytes[i];
+        if ( !utf8 || b < 128 ) {
+          chars[j++] = b;
+        }
+        else if ( b >= 192 && b < 224 && i+1 < len ) {
+          chars[j++] = ( (b & 0x1f) << 6 ) | (bytes[++i] & 0x3f);
+        }
+        else if ( b >= 224 && b < 240 && i+2 < len ) {
+          chars[j++] = ( (b & 0xf) << 12 ) | ( (bytes[++i] & 0x3f) << 6 ) | (bytes[++i] & 0x3f);
+        }
+        else if ( b >= 240 && b < 248 && i+3 < len ) {
+          var c = ( (b & 7) << 18 ) | ( (bytes[++i] & 0x3f) << 12 ) | ( (bytes[++i] & 0x3f) << 6 ) | (bytes[++i] & 0x3f);
+          if ( c <= 0xffff ) {
+            chars[j++] = c;
+          }
+          else {
+            c ^= 0x10000;
+            chars[j++] = 0xd800 | (c >> 10);
+            chars[j++] = 0xdc00 | (c & 0x3ff);
+          }
+        }
+        else {
+          throw new Error("Malformed UTF8 character at byte offset " + i);
+        }
+      }
+
+      var str = '',
+        bs = 16384;
+      for ( var i = 0; i < j; i += bs ) {
+        str += String.fromCharCode.apply( String, chars.slice( i, i+bs <= j ? i+bs : j ) );
+      }
+
+      return str;
+    }
+
+    window.log = function log(str) {
+      var node = document.createTextNode(str);
+      document.body.appendChild(document.createElement('div').appendChild(node));
+    };
+    window.convertToPython = function convertToPython(obj) {
+      if(!obj)
+        return obj;
+
+      if (obj.constructor.name === "Error")
+        return obj;
+
+      if(obj.constructor.name === "Uint8Array")
+        return "@Uint8Array(" + bytes_to_base64(obj) + ")";
+
+      if(typeof obj !== "object")
+        return obj;
+
+      Object.keys(obj).forEach(function(k){
+        if(obj[k].constructor.name === "Uint8Array")
+          obj[k] = "@Uint8Array(" + bytes_to_base64(obj[k]) + ")";
+        else if(typeof obj[k] === "object")
+          obj[k] = convertToPython(obj[k]);
+      });
+
+      return obj;
+    };
+
+    window.convertFromPython = function convertFromPython(obj) {
+      if(!obj)
+        return obj;
+      if(typeof obj === "string" && obj.startsWith('@Uint8Array')){
+        return base64_to_bytes(obj.replace(/@Uint8Array\((.*)\)/, '$1'));
+      }
+
+      if(typeof obj !== "object")
+        return obj;
+      const keys = Object.keys(obj);
+      Object.keys(obj).forEach(function(k){
+        if(typeof obj[k] === "string" && obj[k].startsWith('@Uint8Array'))
+          obj[k] = base64_to_bytes(obj[k].replace(/@Uint8Array\((.*)\)/, '$1'));
+        else if(typeof obj[k] === "object")
+          obj[k] = convertFromPython(obj[k]);
+      });
+
+      return obj;
+    };
+  });
+}
+
 class RemoteClient {
   constructor(remote) {
-    this.remote = remote.init()
+    this.remote = setupHelpers(
+      remote.init()
       .url(`${conf.zeroKit.serviceUrl}/static/v${conf.zeroKit.sdkVersion}/api.html`)
       .timeouts('script', 30000)
-      .execute(function () {
-        function bytes_to_base64(arr){
-          return btoa( bytes_to_string(arr) );
-        }
-
-        function base64_to_bytes(str){
-          return string_to_bytes( atob( str ) );
-        }
-
-        function string_to_bytes ( str, utf8 ) {
-          utf8 = !!utf8;
-
-          var len = str.length,
-            bytes = new Uint8Array( utf8 ? 4*len : len );
-
-          for ( var i = 0, j = 0; i < len; i++ ) {
-            var c = str.charCodeAt(i);
-
-            if ( utf8 && 0xd800 <= c && c <= 0xdbff ) {
-              if ( ++i >= len ) throw new Error( "Malformed string, low surrogate expected at position " + i );
-              c = ( (c ^ 0xd800) << 10 ) | 0x10000 | ( str.charCodeAt(i) ^ 0xdc00 );
-            }
-            else if ( !utf8 && c >>> 8 ) {
-              throw new Error("Wide characters are not allowed.");
-            }
-
-            if ( !utf8 || c <= 0x7f ) {
-              bytes[j++] = c;
-            }
-            else if ( c <= 0x7ff ) {
-              bytes[j++] = 0xc0 | (c >> 6);
-              bytes[j++] = 0x80 | (c & 0x3f);
-            }
-            else if ( c <= 0xffff ) {
-              bytes[j++] = 0xe0 | (c >> 12);
-              bytes[j++] = 0x80 | (c >> 6 & 0x3f);
-              bytes[j++] = 0x80 | (c & 0x3f);
-            }
-            else {
-              bytes[j++] = 0xf0 | (c >> 18);
-              bytes[j++] = 0x80 | (c >> 12 & 0x3f);
-              bytes[j++] = 0x80 | (c >> 6 & 0x3f);
-              bytes[j++] = 0x80 | (c & 0x3f);
-            }
-          }
-
-          return bytes.subarray(0, j);
-        }
-
-        function bytes_to_string ( bytes, utf8 ) {
-          utf8 = !!utf8;
-
-          var len = bytes.length,
-            chars = new Array(len);
-
-          for ( var i = 0, j = 0; i < len; i++ ) {
-            var b = bytes[i];
-            if ( !utf8 || b < 128 ) {
-              chars[j++] = b;
-            }
-            else if ( b >= 192 && b < 224 && i+1 < len ) {
-              chars[j++] = ( (b & 0x1f) << 6 ) | (bytes[++i] & 0x3f);
-            }
-            else if ( b >= 224 && b < 240 && i+2 < len ) {
-              chars[j++] = ( (b & 0xf) << 12 ) | ( (bytes[++i] & 0x3f) << 6 ) | (bytes[++i] & 0x3f);
-            }
-            else if ( b >= 240 && b < 248 && i+3 < len ) {
-              var c = ( (b & 7) << 18 ) | ( (bytes[++i] & 0x3f) << 12 ) | ( (bytes[++i] & 0x3f) << 6 ) | (bytes[++i] & 0x3f);
-              if ( c <= 0xffff ) {
-                chars[j++] = c;
-              }
-              else {
-                c ^= 0x10000;
-                chars[j++] = 0xd800 | (c >> 10);
-                chars[j++] = 0xdc00 | (c & 0x3ff);
-              }
-            }
-            else {
-              throw new Error("Malformed UTF8 character at byte offset " + i);
-            }
-          }
-
-          var str = '',
-            bs = 16384;
-          for ( var i = 0; i < j; i += bs ) {
-            str += String.fromCharCode.apply( String, chars.slice( i, i+bs <= j ? i+bs : j ) );
-          }
-
-          return str;
-        }
-
-        window.log = function log(str) {
-          var node = document.createTextNode(str);
-          document.body.appendChild(document.createElement('div').appendChild(node));
-        };
-        window.convertToPython = function convertToPython(obj) {
-          if(!obj)
-            return obj;
-
-          if (obj.constructor.name === "Error")
-            return obj;
-
-          if(obj.constructor.name === "Uint8Array")
-            return "@Uint8Array(" + bytes_to_base64(obj) + ")";
-
-          if(typeof obj !== "object")
-            return obj;
-
-          Object.keys(obj).forEach(function(k){
-            if(obj[k].constructor.name === "Uint8Array")
-              obj[k] = "@Uint8Array(" + bytes_to_base64(obj[k]) + ")";
-            else if(typeof obj[k] === "object")
-              obj[k] = convertToPython(obj[k]);
-          });
-
-          return obj;
-        };
-
-        window.convertFromPython = function convertFromPython(obj) {
-          if(!obj)
-            return obj;
-          if(typeof obj === "string" && obj.startsWith('@Uint8Array')){
-            return base64_to_bytes(obj.replace(/@Uint8Array\((.*)\)/, '$1'));
-          }
-
-          if(typeof obj !== "object")
-            return obj;
-          const keys = Object.keys(obj);
-          Object.keys(obj).forEach(function(k){
-            if(typeof obj[k] === "string" && obj[k].startsWith('@Uint8Array'))
-              obj[k] = base64_to_bytes(obj[k].replace(/@Uint8Array\((.*)\)/, '$1'));
-            else if(typeof obj[k] === "object")
-              obj[k] = convertFromPython(obj[k]);
-          });
-
-          return obj;
-        };
-      })
+    );
   }
 
   call(method, ...args) {
@@ -240,7 +245,7 @@ class RemoteClient {
   }
 
   continueCodeFlow(url) {
-    return this.remote.newWindow(url, 'idpCodeLogin')
+    return setupHelpers(this.remote.newWindow(url, 'idpCodeLogin'))
       .waitUntil(
         () =>  this.remote.url().then(url => url.value.startsWith(idpCodeRedirect)),
         10000)
@@ -259,7 +264,7 @@ class RemoteClient {
       '&nonce=' + encodeURI(state) +
       '&response_mode=fragment&prompt=none';
 
-    return this.remote.newWindow(authUrl, 'idpHybridLogin')
+    return setupHelpers(this.remote.newWindow(authUrl, 'idpHybridLogin'))
       .waitUntil(
         () => this.remote.url().then(url => url.value.startsWith(cbPath)),
         10000)
